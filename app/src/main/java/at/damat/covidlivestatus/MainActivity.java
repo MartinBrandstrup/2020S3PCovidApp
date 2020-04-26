d89d6c2aca4daa159fe4196fde841495fd29f91e
Vlad Constantinescu <vlad@damat.at>
2020-04-26 18:11:09 +0300

    CI trigger

--- app/src/main/java/at/damat/covidlivestatus/MainActivity.java
+++ app/src/main/java/at/damat/covidlivestatus/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.graphics.Typeface;
-import android.os.Build;
 import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
